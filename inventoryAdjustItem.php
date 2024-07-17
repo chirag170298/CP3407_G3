4477d1dd300f21e491f21884b1458b4693d079c4
Lucas <lucas.obrien@my.jcu.edu.au>
2024-07-17 12:18:18 +1000

    add jquery

--- inventoryAdjustItem.php
+++ inventoryAdjustItem.php
@@ -144,6 +144,8 @@ if ($conn->connect_error) {
         </body>
         </html>
 
+
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script>
             function fetchStock() {
                 // console.log($categories);
