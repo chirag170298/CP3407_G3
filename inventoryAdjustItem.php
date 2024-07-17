3b9e17c75e98622dd5b4b356930fd8468738b0f9
Lucas <lucas.obrien@my.jcu.edu.au>
2024-07-17 12:12:59 +1000

    Add initial PHP scripts for page

---
+++ inventoryAdjustItem.php
@@ -0,0 +1,27 @@
+
+<?php include 'auth.php'; ?>
+
+<?php
+$servername = 'cp3407-website-db.cfumcuommiak.ap-southeast-2.rds.amazonaws.com'; // Replace with your RDS endpoint
+$username = 'CP3407admin';
+$password = 'YFtG]?$4&+k}.WJ';
+$dbname = 'EasyGrocer';
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "SELECT DISTINCT category_Type FROM STOCK";
+$result = $conn->query($sql);
+$categories = [];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $categories[] = $row['category_Type'];
+    }
+} else {
+    echo "0 results";
+}
