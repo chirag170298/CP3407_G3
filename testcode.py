48a6fb1e030e7d8b27579d80e27904d72b4bf94f
Lucas <lucas.obrien@my.jcu.edu.au>
2024-08-06 14:19:59 +1000

    Reorder tests to function correctly

--- testcode.py
+++ testcode.py
@@ -25,14 +25,14 @@ try:
 
         # Insert values
         insert_queries = [
-
-            "INSERT INTO Sales (SaleID, SaleDate, PersonID) VALUES (1000, '2024-08-01', 1006)",
+            "INSERT INTO users (id, username, password) VALUES (8888, 'TestUser1', 'password1')",
+            "INSERT INTO Person (PersonID, FirstName, LastName, StoreID, RoleID, users_id) VALUES (8888, 'John', 'Doe', 1, 1, 8888)",
+            "INSERT INTO Sales (SaleID, SaleDate, PersonID) VALUES (1000, '2024-08-01', 8888)",
             "INSERT INTO HR (idHR, POLICY_COMPLETED, Store_StoreID) VALUES (1, 'Yes', 1)",
             "INSERT INTO Invoice (InvoiceID, SaleID, InvoiceDate, TotalAmount) VALUES (1000, 1000, '2024-08-01', 100.00)",
             "INSERT INTO InvoiceDetails (InvoiceDetailID, InvoiceID, STOCK_ID, Quantity, Price) VALUES (1, 1, 10, 10, 10.00)",
-            "INSERT INTO PERFORMANCE (PersonID, FEEDBACK, TASKS_COMPLETED, ATTENDANCE, EFFICIENCY, TRAINING_COMPLETED) VALUES (1006, 'Good', 5, 100, 90, 2)",
+            "INSERT INTO PERFORMANCE (PersonID, FEEDBACK, TASKS_COMPLETED, ATTENDANCE, EFFICIENCY, TRAINING_COMPLETED) VALUES (8888, 'Good', 5, 100, 90, 2)",
             "INSERT INTO PROMOTIONAL (PROMOTIONAL_ID, PROMOTION_NAME, DESCRIPTION, START_DATE, END_DATE, IMAGE) VALUES (12, 'End of Summer Sale', 'Discounts on summer items', '2024-07-01', '2024-07-31', 'image.jpg')",
-            "INSERT INTO Person (PersonID, FirstName, LastName, StoreID, RoleID, users_id) VALUES (1, 'John', 'Doe', 1, 1, 1)",
             "INSERT INTO Role (RoleID, RoleName) VALUES (4, 'District Manager')",
             "INSERT INTO Roster (RosterID, Date, ShiftID, users_id) VALUES (1, '2024-08-01', 1, 1)",
             "INSERT INTO SUPPLIER (SUPPLIER_ID, SUPPLIER_NAME, SUPPLIER_EMAIL, SUPPLIER_CONTACT_NUM) VALUES (1, 'Supplier A', 'contact@suppliera.com', '1234567890')",
@@ -40,7 +40,6 @@ try:
             "INSERT INTO Shift (ShiftID, ShiftType) VALUES (2000, 'Test shift')",
             "INSERT INTO Store (StoreID, StoreName) VALUES (2, 'Second Main Store')",
             "INSERT INTO TIMESHEET (TIMESHEET_ID, Roster_RosterID, CLOCK_IN, CLOCK_OUT) VALUES (1, 1, '08:00:00', '16:00:00')",
-            "INSERT INTO users (id, username, password) VALUES (1010, 'TestUser1', 'password1')"
         ]
 
         for query in insert_queries:
@@ -53,7 +52,7 @@ try:
             "DELETE FROM HR WHERE idHR = 1",
             "DELETE FROM Invoice WHERE InvoiceID = 1000",
             "DELETE FROM InvoiceDetails WHERE InvoiceDetailID = 1",
-            "DELETE FROM PERFORMANCE WHERE PersonID = 1006",
+            "DELETE FROM PERFORMANCE WHERE PersonID = 8888",
             "DELETE FROM PROMOTIONAL WHERE PROMOTIONAL_ID = 12",
             "DELETE FROM Person WHERE PersonID = 1",
             "DELETE FROM Role WHERE RoleID = 4",
