c8c0c6284a9b50dae862e36d7dedff568094ca55
Lucas <lucas.obrien@my.jcu.edu.au>
2024-08-06 13:36:50 +1000

    Add and complete Fetchfeedback

---
+++ fetchFeedback.php
@@ -0,0 +1,40 @@
+
+<?php
+
+$servername = 'cp3407-website-db.cfumcuommiak.ap-southeast-2.rds.amazonaws.com'; // Replace with your RDS endpoint
+$username = 'CP3407admin';
+$password = 'YFtG]?$4&+k}.WJ';
+$dbname = 'EasyGrocer';
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM FEEDBACK";
+
+$result = $conn->query($sql);
+
+$feedbackData = array();
+
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        $feedbackData[] = $row;
+    }
+} else {
+    $feedbackData = ['error' => 'No results found'];
+}
+header('Content-Type: application/json');
+echo json_encode($feedbackData);
+$conn->close();
+
+exit;
+
+
+
+
+?>
